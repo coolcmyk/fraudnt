af65e50955822e186b191fd75744b03012a84149
coolcmyk <ryanadi001@gmail.com>
2024-08-14 22:01:45 +0700

    firebase config

---
+++ FE/public/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <div className='text-white font-poppins'>
+      sex
+    </div>
+  );
+};
+
+export default HomePage;
