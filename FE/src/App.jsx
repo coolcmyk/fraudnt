1ccf571e1b674768528ca7d99be96dd2898c3c44
coolcmyk <ryanadi001@gmail.com>
2024-08-16 20:23:05 +0700

    fixed deployment

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,40 +1,40 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
-// import './App.css'
-// import HomePage from './components/HomePage'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import './App.css'
+import HomePage from './components/HomePage'
 
-// const App = () => (
-//       <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Navigate to={"/home"} />} />
+const App = () => (
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={"/home"} />} />
 
-//         <Route path="/home" element={<HomePage />} />
-//       </Routes>
-//     </BrowserRouter>
-// );
-
-
-
-// export default App;
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';  
-
-import Homepage from '../../FE/src/components/HomePage.jsx';
-
-function App() {
-return (
- <BrowserRouter>
-   <Routes>
-     <Route path="/" element={<Homepage />} />
-     {/* Other routes */}
-   </Routes>
- </BrowserRouter>
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
 );
-}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+
+export default App;
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';  
+
+// import Homepage from '../../FE/src/components/HomePage.jsx';
+
+// function App() {
+// return (
+//  <BrowserRouter>
+//    <Routes>
+//      <Route path="/" element={<Homepage />} />
+//      {/* Other routes */}
+//    </Routes>
+//  </BrowserRouter>
+// );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
